9654002bfb4047b95648b2927613fb05a3da1fa4
Liamsalass <liamsalass@yahoo.com>
2023-10-20 13:43:47 -0400

    gammas greater than 1

--- Lab2/src/adain.sh
+++ Lab2/src/adain.sh
@@ -4,10 +4,10 @@
 #SBATCH --partition=Sasquatch
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
-#SBATCH --output=logs/gamma1.20.out
-#SBATCH --error=logs/gamma1.20.err
+#SBATCH --output=logs/gamma2.50.out
+#SBATCH --error=logs/gamma2.50.err
 #SBATCH --time=6:00:00
 
-CUDA_VISIBLE_DEVICES=3
+CUDA_VISIBLE_DEVICES=2
 
-python3 train.py -content_dir /ingenuity_NAS/dataset/public/coco/images/train2017/ -style_dir /ingenuity_NAS/dataset/public/wikiart/ -gamma 1.20 -e 20 -b 20 -l encoder.pth -s decoder_g120_10k.pth -p decoder_g120_10k.png -cuda Y
+python3 train.py -content_dir /ingenuity_NAS/dataset/public/coco/images/train2017/ -style_dir /ingenuity_NAS/dataset/public/wikiart/ -gamma 2.5 -e 20 -b 50 -l encoder.pth -s decoder_g250_10k.pth -p decoder_g250_10k.png -cuda Y
