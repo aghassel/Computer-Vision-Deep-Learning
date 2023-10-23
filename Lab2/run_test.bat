f9b6035b3a7034acd273e21c41b13e687aedd1d3
Liamsalass <liamsalass@yahoo.com>
2023-10-23 19:57:41 -0400

    updating imgs

--- Lab2/run_test.bat
+++ Lab2/run_test.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set CONTENT_IMG_PATH="images/content/ali.png"
+set CONTENT_IMG_PATH="images/content/baboon.jpg"
 set STYLE_IMG_PATH_IDK="images/style/0a585acb9d7134c0b39656a588527385c.jpg"
 set STYLE_IMG_PATH_WARHOL="images/style/Andy_Warhol_97.jpg"
 set STYLE_IMG_PATH_BRUSH="images/style/brushstrokes.jpg"
@@ -8,8 +8,8 @@ set STYLE_IMG_PATH_MARC="images/style/chagall_marc_1.jpg"
 set STYLE_IMG_PATH_PERS="images/style/the-persistence-of-memory-1931.jpg"
 set STYLE_IMG_PATH_NIGHT="images/style/starynight.jpg"
 set ENCODER_PATH="models/encoder.pth"
-set DECODER_PATH="models/decoder_g10.0_10k.pth"
-set OUTPUT_PATH="output/10k/gamma10/ali/"
+set DECODER_PATH="models/decoder_g20_10k.pth"
+set OUTPUT_PATH="output/10k/gamma2.0/baboon/"
 set CUDA="N"
 
 echo "Running test.py"
