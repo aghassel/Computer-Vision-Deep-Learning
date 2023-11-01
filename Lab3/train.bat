2b3abc481a8925df8962667c47d811d78a9ba1f8
LiamSalass <liamsalass@yahoo.com>
2023-11-01 17:35:48 -0400

    Basic train script

---
+++ Lab3/train.bat
@@ -0,0 +1,7 @@
+@echo off
+
+echo "Activating conda env"
+call conda activate ELEC475
+
+echo "Training the model"
+python train.py --batch_size 128 --epochs 50 --lr 0.001 --classes 100 --encoder encoder.pth --frontend vit.pth --save_plot vit_plot.png
