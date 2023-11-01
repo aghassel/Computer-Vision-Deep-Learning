4fe48dd37f1f2d00e8d1dee1a89b8d14448fcfa7
aghassel <aghassel@gmail.com>
2023-11-01 12:43:59 -0400

    finished vanilla lab 3

---
+++ Lab3/test.py
@@ -0,0 +1,68 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from vanilla import VanillaFrontend, VGG 
+import argparse
+
+def accuracy(output, target, topk=(1,)):
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+def main():
+    parser = argparse.ArgumentParser(description="Testing")
+    parser.add_argument('--batch_size', type=int, default=4)
+    parser.add_argument('--classes', type=int, default=100)
+    parser.add_argument('--encoder', type=str)
+    parser.add_argument('--frontend', type=str)
+    args = parser.parse_args()
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(f"Device being used: {device}")
+
+    transform = transforms.Compose([
+        transforms.Resize((32, 32)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ])
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=False, num_workers=2)
+
+    encoder = VGG.encoder
+    encoder.load_state_dict(torch.load(args.encoder, map_location=device))  
+    encoder = encoder.to(device)
+    model = VanillaFrontend(encoder, num_classes=args.classes).to(device)
+    model.load_state_dict(torch.load(args.frontend, map_location=device))
+    model.eval()
+
+    top1_accuracy = 0.0
+    top5_accuracy = 0.0
+    num_batches = 0
+
+    with torch.no_grad():
+        for i, data in enumerate(testloader):
+            images, labels = data[0].to(device), data[1].to(device)
+            outputs = model(images)
+            acc1, acc5 = accuracy(outputs, labels, topk=(1, 5))
+            top1_accuracy += acc1[0]
+            top5_accuracy += acc5[0]
+            num_batches += 1
+
+    top1_avg_accuracy = top1_accuracy / num_batches
+    top5_avg_accuracy = top5_accuracy / num_batches
+
+    print(f'Top 1 accuracy: {top1_avg_accuracy:.2f}%, Top 5 accuracy: {top5_avg_accuracy:.2f}%')
+
+if __name__ == "__main__":
+    main()
---
+++ Lab3/vanilla.py
@@ -0,0 +1,62 @@
+#   Adapted from:
+#       https://github.com/naoto0804/pytorch-AdaIN
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.optim as optim
+
+class VGG:
+        encoder = nn.Sequential(
+        nn.Conv2d(3, 3, (1, 1)),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(3, 64, (3, 3)),
+        nn.ReLU(),  # relu1-1
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(64, 64, (3, 3)),
+        nn.ReLU(),  # relu1-2
+        nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(64, 128, (3, 3)),
+        nn.ReLU(),  # relu2-1
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(128, 128, (3, 3)),
+        nn.ReLU(),  # relu2-2
+        nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(128, 256, (3, 3)),
+        nn.ReLU(),  # relu3-1
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 256, (3, 3)),
+        nn.ReLU(),  # relu3-2
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 256, (3, 3)),
+        nn.ReLU(),  # relu3-3
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 256, (3, 3)),
+        nn.ReLU(),  # relu3-4
+        nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 512, (3, 3)),
+        nn.ReLU(),  # relu4-1, this is the last layer used
+    )
+        
+class VanillaFrontend(nn.Module):
+    def __init__(self, encoder, num_classes=100):
+        super(VanillaFrontend, self).__init__()
+
+        self.encoder = encoder  
+
+        self.frontend = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1)),   # Convert the 512 feature maps to 1x1x512
+            nn.Flatten(),                   # Flatten the 1x1x512 to 512x1
+            nn.Linear(512, num_classes)  
+        )
+
+    def forward(self, x):
+        
+        with torch.no_grad():
+            x = self.encoder(x)  
+
+        return self.frontend(x) 
