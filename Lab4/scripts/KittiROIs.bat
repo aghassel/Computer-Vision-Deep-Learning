e2accbcb7c572ace9b23f6a289490ef07bfc87c3
LiamSalass <liamsalass@yahoo.com>
2023-11-07 15:32:28 -0500

    Add train and test dataset generation scripts for KittiROIs.bat

--- Lab4/scripts/KittiROIs.bat
+++ Lab4/scripts/KittiROIs.bat
@@ -4,6 +4,12 @@ call conda activate ELEC475
 
 cd ..
 
+echo "Generating Train dataset..."
+
 python Kitti/KittiToYodaROIs.py -i data/Kitti8 -o data/Kitti8_ROIs/train -m train -cuda y -IoU 0.02 -d y 
 
+echo "Generating Test dataset..."
+
 python Kitti/KittiToYodaROIs.py -i data/Kitti8 -o data/Kitti8_ROIs/test -m test -cuda y -IoU 0.02 -d y
+
+echo "Done!"
