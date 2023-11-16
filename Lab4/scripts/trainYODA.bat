ed24f0a42ef71732197a3cf23332a57a1d39286a
Liamsalass <liamsalass@yahoo.com>
2023-11-15 20:22:48 -0500

    Update YODA classifier model name to resnet18 and reduce number of threads to 4.

--- Lab4/scripts/trainYODA.bat
+++ Lab4/scripts/trainYODA.bat
@@ -4,6 +4,6 @@ cd ..
 
 call conda activate ELEC475
 
-python train_YODA_classifier.py --batch_size 75 --epochs 25 --lr 0.001 --model_name mod --momentum 0.9 --data_dir data/Kitti8_ROIs/ --save_dir models/ --cuda True --loss_plot_dir graphs/ --threads 6
+python train_YODA_classifier.py --batch_size 75 --epochs 25 --lr 0.001 --model_name resnet18 --momentum 0.9 --data_dir data/Kitti8_ROIs/ --save_dir models/ --cuda True --loss_plot_dir graphs/ --threads 4
 
 cd scripts
