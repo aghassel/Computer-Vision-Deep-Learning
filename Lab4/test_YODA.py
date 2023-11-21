6d929b77588817ca98a2e36888245671c1e14cf7
LiamSalass <liamsalass@yahoo.com>
2023-11-20 21:33:56 -0500

    Add image directory to .gitignore and update test_YODA.py

--- Lab4/test_YODA.py
+++ Lab4/test_YODA.py
@@ -7,6 +7,7 @@ from Kitti.KittiAnchors import Anchors
 import matplotlib.pyplot as plt
 import cv2
 from PIL import Image
+from tqdm import tqdm
 
 def test(args):
     device = torch.device('cuda' if args.cuda and torch.cuda.is_available() else 'cpu')
@@ -43,59 +44,75 @@ def test(args):
 
     ious = []
 
+    count = 0
+    for item in tqdm(enumerate(dataset), total=len(dataset), desc="Testing", unit="images", leave=False):
+        idx, (image, label) = item
+        original_image_size = image.shape[:2]
+        idx = dataset.class_label['Car']
+        car_ROIs = dataset.strip_ROIs(class_ID=idx, label_list=label)
+        ROIs, boxes = anchors.get_anchor_ROIs(image, anchors.calc_anchor_centers(image.shape, anchors.grid), anchors.shapes)
 
-    with torch.no_grad():
-        for item in enumerate(dataset):
-            idx = item[0]
-            image = item[1][0]
-            label = item[1][1]
+        img_copy = image.copy()
+        transformed_ROIs = []
 
-            idx = dataset.class_label['Car']
-            car_ROIs = dataset.strip_ROIs(class_ID=idx, label_list=label)
-
-            anchor_centers = anchors.calc_anchor_centers(image.shape, anchors.grid)
-            anchor_ROIs, boxes = anchors.get_anchor_ROIs(image, anchor_centers, anchors.shapes)
-
-            image_copy = image.copy()
-
-            ROI_IoUs = []
-            for idx in range(len(anchor_ROIs)):
-                ROI = anchor_ROIs[idx]
-                ROI = Image.fromarray(ROI)
-                ROI = transform(ROI)
-                ROI = ROI.unsqueeze(0)
-                ROI = ROI.to(device)
-
-                output = model(ROI)
-                output = torch.sigmoid(output)
-                output = output.item()
-
-                if output > IoU_threshold:
-                    ROI_IoUs += [anchors.calc_IoU(boxes[idx], car_ROIs)]
-                else:
-                    ROI_IoUs += [0]
-            
-            ious += [max(ROI_IoUs)]
-
-            print('Image: ', idx, ' IoU: ', max(ROI_IoUs))
-        
-        print('Average IoU: ', sum(ious)/len(ious))
+        roi_counts = 0
+        for ROI in ROIs:
+            ROI = Image.fromarray(ROI)
+            ROI = transform(ROI)
+            roi_counts += 1
+            transformed_ROIs.append(ROI)
     
+
+        if roi_counts > 50:
+            batch1 = transformed_ROIs[:75]
+            batch2 = transformed_ROIs[75:]
+
+            ROIs_tensor_batch1 = torch.stack(batch1).to(device)
+            ROIs_tensor_batch2 = torch.stack(batch2).to(device)
+
+            outputs_batch1 = model(ROIs_tensor_batch1).squeeze()
+            outputs_batch2 = model(ROIs_tensor_batch2).squeeze()
+
+            outputs = torch.cat((outputs_batch1, outputs_batch2))
+        else:
+            ROIs_tensor = torch.stack(transformed_ROIs).to(device)
+            outputs = model(ROIs_tensor).squeeze()
+
+        outputs = torch.sigmoid(outputs)
+        outputs = torch.round(outputs)
+
+        # Rest of the code...
+
+        ROIs_tensor = torch.stack(transformed_ROIs).to(device)
+
+        outputs = model(ROIs_tensor).squeeze()
+        outputs = torch.sigmoid(outputs)
+        outputs = torch.round(outputs)
+
+        for j in range(len(boxes)):
+            box = boxes[j]
+            pred = outputs[j].item()
+            if pred == 1:
+                pt1 = box[0]
+                pt2 = box[1]
+                pt1 = (int(pt1[1]), int(pt1[0]))
+                pt2 = (int(pt2[1]), int(pt2[0]))
+                cv2.rectangle(img_copy, pt1, pt2, (0, 255, 255))
+
+                iou = anchors.calc_max_IoU(box, car_ROIs)
+                ious.append(iou)
+                cv2.putText(img_copy, f"{iou:.2f}", (pt1[0], pt1[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+
+        if args.save_dir and args.save_imgs:
+            save_path = os.path.join(args.save_dir, f"processed_{count}.png")  # Changed from idx to count
+            cv2.imwrite(save_path, img_copy)
+        count += 1
+
+    # Calculate and print mean IoU
+    mean_iou = sum(ious) / len(ious) if ious else 0
+    print(f"Mean IoU: {mean_iou}")
+
     return
-
-
-
-
-            
-
-            
-      
-
-
-
-
-
-    
 
 
 
@@ -104,21 +121,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train YODA classifier')
     parser.add_argument('--model_name', type=str, default='mod2', help='model name (default: resnet18)')
     parser.add_argument('--data_dir', type=str, default='data/Kitti8/', help='path to dataset')
+    parser.add_argument('--save_imgs', type=bool, default=False, help='save images with ROIs drawn on them')
     parser.add_argument('--save_dir', type=str, default='images/', help='path to save images')
-    parser.add_argument('--cuda', type=bool, default=True, help='enables CUDA for proces')
-    parser.add_argument('--IoU', metavar='IoU_threshold', type=float, default=0.02, help='[0.02]')
+    parser.add_argument('--cuda', type=bool, default=False, help='enables CUDA for proces')
     args = parser.parse_args()
 
-    IoU_threshold = 0.02
-    if args.IoU != None:
-        IoU_threshold = float(args.IoU)
 
     print('Parameters:')
     print('\tModel name:\t', args.model_name)
     print('\tModel Path:\t', os.path.join('models', args.model_name + '.pth'))
     print('\tData dir:\t', args.data_dir)
     print('\tSave dir:\t', args.save_dir)
-    print('\tIoU:\t\t', IoU_threshold)
 
     print('\n')
 
